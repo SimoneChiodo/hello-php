00ec4d1afd84ef2e305df30b7147289fa9370fa7
SimoneChiodo <simochi.005@gmail.com>
2025-12-15 16:49:21 +0100

    Add code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Per avviare il codice PHP su un server locale usare nel terminale: 
 `php -S localhost:8000`  
-*(`-S` indica che deve lanciare il **Server**)*
+*(`-S` indica che deve lanciare il **Server**)*  
+
+Questo perché PHP è un codice Server-Side, che quindi viene elaborato sul Server
--- index.php
+++ index.php
@@ -1,15 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <p>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello PHP</title>
+  </head>
+  <body>
+    <h1>
+      <?= 'Metodo inline'?> <!-- Struttura inline (dove non si cambia "php" con "=") -->
+    </h1> 
+
+    <p>
+      <?php
+        echo 'Metodo multilinea';
+      ?>
+    </p>
+
+    <!-- Gli apici '' permettono di contenere stringhe semplici.
+    Per contenere caratteri speciali o variabili, bisogna usare le virgolette "" -->
     <?php
-      echo 'HelloWorld';
+      echo "\t<ul>\n";
+      echo "\t\t\t<li>Elemento 1</li>\n";
+      echo "\t\t\t<li>Elemento 2</li>\n";
+      echo "\t\t\t<li>Elemento 3</li>\n";
+      echo "\t\t</ul>\n";
     ?>
-  </p>
-</body>
+
+    <!-- Per concatenare più stringhe si può usare il carattere . o ,
+    Per convenzione si usa il . -->
+    <?php
+      echo "ciao1",
+        "ciao2",
+        "ciao3\n";
+
+      echo "ciao4".
+        "ciao5".
+        "ciao6\n";
+
+      // Si può anche usare <<< per creare codice multilinea.
+      // Questo metodo si chiama heredoc
+      echo <<<END
+        <ul>
+          <li>Nuovo Elemento 1</li>
+          <li>Nuovo Elemento 2</li>
+          <li>Nuovo Elemento 3</li>
+        </ul>
+      END;
+    ?>
+
+    <!-- Al posto di echo si può anche usare la funzione print(). 
+    Quest'ultima ritorna un valore true se la sua operazione di scrittura ha avuto successo oppure false in caso contrario. -->
+    <?php
+      print("Questa è una print");
+    ?>
+
+    <!-- I commenti in PHP si creano con i caratteri #, //, /**/ -->
+    <?php
+      # Commento 1 
+      // Commento 2
+      /* Commento multilinea 1
+      Commento multilinea 2 */
+    ?>
+  </body>
 </html>
