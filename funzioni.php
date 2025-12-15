886dbb79e8d1f2303b23c24ec0da34dda4d4169f
SimoneChiodo <simochi.005@gmail.com>
2025-12-15 18:02:06 +0100

    Add funzioni.php

---
+++ funzioni.php
@@ -0,0 +1,43 @@
+<?php
+
+  function test() {
+    global $x;
+    $x = 100;
+    $y = 100;
+
+    $x++; // Questa non aumenta a ogni ciclo, perché il suo valore viene sovrascritto a inizio funzione
+
+    return $var = 20;
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <!-- Usare una funzione -->
+    <h1>
+      <?php
+        echo test();
+        echo test();
+      ?>
+    </h1>
+    <!-- Usare variabili globali -->
+    <h2>
+      <?php
+        echo $x;
+      ?>
+    </h1>
+    <!-- Usare variabili con scope -->
+    <h2>
+      <?php
+        echo $y;
+      ?>
+    </h1>
+  </body>
+</html>
